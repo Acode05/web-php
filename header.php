fe3d01cf62b010c53648b20e7ceea140f3b9ccd4
Acode05 <275999882+Acode05@users.noreply.github.com>
2026-05-18 04:59:32 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo isset($page_title) ? $page_title : 'Pawsy'; ?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class= "animals" > </div>
+
+    <div class="header">
+        <span class="logo-text">
+            <img src="logo.jpg" class="logo-img">Pawsy
+        </span>
+        <a href="login.php" class="header-link">login</a>
+    </div>
